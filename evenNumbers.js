0ef9aaab5445cd491ef2639b1fb5dc0126e732a0
varsha barki <108147219+Varshakiran@users.noreply.github.com>
2022-07-02 21:10:30 +0530

    Add files via upload

---
+++ evenNumbers.js
@@ -0,0 +1,20 @@
+// for(let i=0; i<100; i++) {
+//     if (i%2 === 0) {
+//         console.log('even numbers', i)
+//     }
+// }
+
+// for(let i=0; i<100; i = i+2) {
+    
+//         console.log( i);
+   
+// }
+
+let sum = 0;
+for(let i=0; i<100; i = i+2) {
+    
+    sum += i;
+   
+
+}
+console.log( sum);
