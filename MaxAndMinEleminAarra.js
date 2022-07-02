8b6ed9bd870d9dfeff8f23b0478da0e781d50e22
varsha barki <108147219+Varshakiran@users.noreply.github.com>
2022-07-02 20:28:50 +0530

    Add files via upload

--- MaxAndMinEleminAarra.js
+++ MaxAndMinEleminAarra.js
@@ -1,6 +1,6 @@
 function minValue(arr) {
     let minValue = arr[0];
-    for(let i=0;i<arr.length-1;i++) {
+    for(let i=0;i<=arr.length-1;i++) {
         if(arr[i]<minValue) {
             minValue = arr[i];
         }
@@ -9,3 +9,14 @@ function minValue(arr) {
 }
 
 console.log(minValue([1,34,5,6,67]));
+function maxValue(arr) {
+    let maxValue = arr[0];
+    for(let i=0;i<=arr.length-1;i++) {
+        if(arr[i]>maxValue) {
+            maxValue = arr[i];
+        }
+    }
+    return maxValue;
+}
+
+console.log(maxValue([1,34,5,6,67]));
